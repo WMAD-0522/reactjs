a2e363a3b4874035701d65565bfe6939b1c62f77
rei_kaji <st.r.z.rei@gmail.com>
2023-03-08 21:05:27 -0800

    update

--- week3/day2-3/tenzie-game/src/App.js
+++ week3/day2-3/tenzie-game/src/App.js
@@ -1,35 +1,34 @@
-import './App.css';
+import "./App.css";
 import Die from "./components/Die/Die";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 function App() {
-
-  const [dice, setDice] = useState(allNewDice())
+  const [dice, setDice] = useState(allNewDice());
 
   function generateNewDie() {
-      return { 
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      }
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
   }
 
-  function allNewDice(){
+  function allNewDice() {
     const newDice = [];
 
-    for(let i = 0; i < 10; i++){
-      newDice.push(generateNewDie())
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
-  function rollDice(){
-    // Challenge 3: 
+  function rollDice() {
+    // Challenge 3:
     // You will setDice, but before you need to check isHeld propery, if its true, you will not give a new random number for die, but if isHeld is false you will generate a new random number for this die.
   }
 
-  function holdDice(id){
-    // Challenge 2: Once i click the dice, I need to 
+  function holdDice(id) {
+    // Challenge 2: Once i click the dice, I need to
     // change isHeld property as toggle
   }
 
@@ -40,13 +39,14 @@ function App() {
 
   return (
     <main className="main">
-      <h1 className='main--title'>Tenzies</h1>
-      <p className='main--content'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <h1 className="main--title">Tenzies</h1>
+      <p className="main--content">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
 
-      <div className='main--list'>
-        {
-          dieElements
-        }
+      <div className="main--list">
+        {dieElements}
         {/* <Die value="1"/>
         <Die value="1"/>
         <Die value="1"/>
@@ -58,7 +58,9 @@ function App() {
         <Die value="5"/>
         <Die value="4"/> */}
       </div>
-      <button className='main--action' onClick={rollDice}>Roll</button>
+      <button className="main--action" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
